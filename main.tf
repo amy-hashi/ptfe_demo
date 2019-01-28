002eb63d66362ccc4f976703db88ded7ec55d898
Amy Brown <amy@hashicorp.com>
2019-01-28 17:13:55 -0600

    comment out app nonsense

--- main.tf
+++ main.tf
@@ -45,46 +45,46 @@ resource "aws_route53_record" "amy-ptfe-demo" {
   ttl = "300"
   records = ["${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)}"]
   
-  connection {
-    type = "ssh"
-    host = "${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)}"
-    user = "ubuntu"
-    private_key = "${var.aws_pem}"
-    agent = false
-    }
-  
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir /tmp/ptfe-install",
-      "sudo apt-get install -y software-properties-common",
-      "sudo add-apt-repository -y universe",
-      "sudo add-apt-repository -y ppa:certbot/certbot",
-      "sudo apt-get install -y certbot",
-      "sudo certbot certonly --standalone --non-interactive --agree-tos --email ${var.email_address} -d ${element(aws_route53_record.amy-ptfe-demo.*.fqdn, count.index)}",
-      ]
-    }
-  
-  provisioner "file" {
-    source = "${var.json_location}"
-    destination = "/tmp/ptfe-install/settings.json"
-    }
-  
-  provisioner "file" {
-    source = "${var.replicated_conf}"
-    destination = "/etc/replicated.conf"
-    }
-  
-  provisioner "file" {
-    source = "${var.license}"
-    destination = "/tmp/ptfe-install/license.rli"
-    }
-  
-  provisioner "remote-exec" {
-    inline = [
-      "cd /tmp/ptfe-install; curl -o install.sh https://install.terraform.io/ptfe/stable",
-      "bash /tmp/ptfe-install/install.sh no-proxy private-address=${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)} public-address=${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)}"
-      ]
-    }
+  #connection {
+  #  type = "ssh"
+  #  host = "${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)}"
+  #  user = "ubuntu"
+  #  private_key = "${var.aws_pem}"
+  #  agent = false
+  #  }
+  #
+  #provisioner "remote-exec" {
+  #  inline = [
+  #    "mkdir /tmp/ptfe-install",
+  #    "sudo apt-get install -y software-properties-common",
+  #    "sudo add-apt-repository -y universe",
+  #    "sudo add-apt-repository -y ppa:certbot/certbot",
+  #    "sudo apt-get install -y certbot",
+  #    "sudo certbot certonly --standalone --non-interactive --agree-tos --email ${var.email_address} -d ${element(#aws_route53_record.amy-ptfe-demo.*.fqdn, count.index)}",
+  #    ]
+  #  }
+  #
+  #provisioner "file" {
+  #  source = "${var.json_location}"
+  #  destination = "/tmp/ptfe-install/settings.json"
+  #  }
+  #
+  #provisioner "file" {
+  #  source = "${var.replicated_conf}"
+  #  destination = "/etc/replicated.conf"
+  #  }
+  #
+  #provisioner "file" {
+  #  source = "${var.license}"
+  #  destination = "/tmp/ptfe-install/license.rli"
+  #  }
+  #
+  #provisioner "remote-exec" {
+  #  inline = [
+  #    "cd /tmp/ptfe-install; curl -o install.sh https://install.terraform.io/ptfe/stable",
+  #    "bash /tmp/ptfe-install/install.sh no-proxy private-address=${element(aws_instance.amy-ptfe-demo.*.public_ip, #count.index)} public-address=${element(aws_instance.amy-ptfe-demo.*.public_ip, count.index)}"
+  #    ]
+  #  }
 }
 
 #resource "null_resource" "provision_ptfe" {  
