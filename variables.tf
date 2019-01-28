02db379d6e2eb2b928501813c0de04c545919369
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2019-01-28 16:26:35 -0600

    moar variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "aws_pem" {}
+variable "instance_count" {
+  default = 1
+  }
+variable "json_location" {
+  default = "application-install/settings.json"
+  }
+variable "replicated_conf" {
+  default = "application-install/replicated.conf"
+  }
+variable "license" {
+  default = "application-install/amy.rli"
+  }
