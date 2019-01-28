5d6b5d1c8e07fdbf84f3c672a9e010a80e224bad
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2019-01-28 10:49:45 -0600

    birthday

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-instance/ubuntu-bionic-18.04-amd64-*"]
+    }
+    
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+    }
+    
+  owners = ["099720109477"]
+  }
+  
+resource "aws_instance" "amy-ptfe-demo" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "m5a.large"
+  key_name = "amy-ohio"
+  security_groups = ["sg-0753511d92cf43cc5"]
+  subnet_id = "subnet-0df3e7ca2603dc1ac"
+  associate_public_ip_address = true
+  volume_tags {
+    volume_size = "50"
+    }
+  count = 1
+  tags {
+    "Owner" = "Amy Brown"
+  }
+}
